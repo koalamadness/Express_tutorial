95d9587de3979b4a1e9782eaf94b1e4759fd4c04
YeQiu <paulhiram@hotmail.com>
2025-05-14 17:58:34 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,23 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.text());
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
-app.get('/', (req, res) => {
-    res.send('Heavy is the crown');
-});
-
-app.get('/miarchivo', (req, res) => {
-    res.sendFile('Actions.png', {
-        root: __dirname
-    });
-});
-
-app.get('/user', (req, res) => {
-    res.json({"name":"crown"});
-});
-
-app.get('/isAlive', (req, res) => {
-    res.sendStatus(204);
+app.post('/user', (req, res) => {
+    console.log(req.body);
+    res.send('New user created');
 });
 
 app.listen(3500);
