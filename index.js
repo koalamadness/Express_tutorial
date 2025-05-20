dbdd502ab34c0a255855305ffc15b87d6f19b014
YeQiu <paulhiram@hotmail.com>
2025-05-19 20:06:10 -0600

    Params

--- index.js
+++ index.js
@@ -2,15 +2,36 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.text());
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
 
-app.post('/user', (req, res) => {
-    console.log(req.body);
-    res.send('New user created');
+app.get('/hello/:user', (req, res) => {
+    res.send(`New user ${req.params.user.toLowerCase()} ${req.params.user} `);
 });
 
-app.listen(3500);
+app.get('/add/:x/:y', (req, res) => {
+    const {x, y} = req.params;
+    const result = parseInt(x) + parseInt(y);
+    res.send(`Result: ${result}`);
+    
+});
 
-console.log('Perrida en ${3500}');
+app.get('/hello/:user/photo', (req, res) => {
+    if(req.params.user === "jaimico") {
+        return res.sendFile('./Actions.png', {
+            root: __dirname
+        });
+    }
+
+    res.send(`${req.params.user} access not allowed`);
+});
+
+
+app.get('/user/:user/age/:age', (req, res) => {
+    const age = parseInt(req.params.age);
+    if(age >= 18){
+        res.send(`The user ${req.params.user} is ${age} years old and is an adult`);
+    }
+    else{
+        res.send(`The user ${req.params.user} is ${age} years old and is a minor`);
+    }
+    
+});
