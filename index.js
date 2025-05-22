41591b0c9e3177b54b2db37ab720e3403f6aa147
YeQiu <paulhiram@hotmail.com>
2025-05-21 18:23:50 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
-
+// use path
 app.use(express.json());
 app.use(morgan('dev'));
+
+
+app.get('/note.txt', (req, res) => {
+    res.send(`Jaimicazo pero exquisito`);
+});
+
 
 app.all('/about', (req, res) => {
     console.log(req.body);
@@ -12,19 +18,14 @@ app.all('/about', (req, res) => {
 });
 
 
-app.use((req, res, next) => {
-
-    if(req.query.login === 'jaimico'){
-        next();
-    }
-    else{
-        res.send("Nel");
-    }
-});
-
 app.get('/all', (req, res) => {
     res.send(`DOTA`);
 });
+
+app.use("/public", express.static('./public'));
+app.use("/uploads", express.static('./uploads'));
+//app.use("/uploads", express.static(path.join(__dirname, 'uploads')));
+
 
 
 app.listen(3500);
